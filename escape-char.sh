52fe4cd0338ae5c9a467157962691daa7dea6cd1
unknown <rmtestard@gmail.com>
2020-01-28 21:02:29 +0100

    I have added commands to espace characters and using wildcard

---
+++ escape-char.sh
@@ -0,0 +1,8 @@
+# Keeps the quotes
+echo "Hello   \"World\""
+# Keep the spacing
+echo "Hello   " World ""
+
+echo "This is \\ a backslash"
+
+echo "This is \" a quote and this is \\ a backslash"
---
+++ wildcards.sh
@@ -0,0 +1,8 @@
+# copy files to another directory
+cp /tmp/a/* /tmp/b/
+cp /tmp/a/*.txt /tmp/b/
+cp /tmp/a/*.html /tmp/b/
+
+# rename files extension
+mv *.txt *.bak
+
